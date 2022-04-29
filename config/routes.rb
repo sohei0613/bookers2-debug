a04e0e750347159b3a5a1ddbd28fd253ac202ad1
EC2 Default User <ec2-user@ip-172-31-10-177.ec2.internal>
2022-04-29 11:36:27 +0000

    [Finish]応用4

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
+  # ネストさせる
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   get "home/about"=>"homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
